ce70af8b374ecfb7f6685741c9c552bf3540553a
jeisaraja <jeisa.raja.jrt@gmail.com>
2022-05-11 00:10:26 +0700

    second commit

--- tutorial.go
+++ tutorial.go
@@ -9,3 +9,22 @@ func main() {
 	number := "nulls"
 	fmt.Printf("%T", number)
 }
+
+// := digunakan untuk membuat variabel tanpa data types (mirip seperti python, go yang akan meng assign data types nya)
+// 'Go ruh' digunakan untuk menjalankan program go
+// 'Go build' digunakan untuk compile program go
+
+// %s	the uninterpreted bytes of the string or slice
+// %q	a double-quoted string safely escaped with Go syntax
+// %x	base 16, lower-case, two characters per byte
+// %X	base 16, upper-case, two characters per byte
+
+// %b	base 2
+// %c	the character represented by the corresponding Unicode code point
+// %d	base 10
+// %o	base 8
+// %O	base 8 with 0o prefix
+// %q	a single-quoted character literal safely escaped with Go syntax.
+// %x	base 16, with lower-case letters for a-f
+// %X	base 16, with upper-case letters for A-F
+// %U	Unicode format: U+1234; same as "U+%04X"
